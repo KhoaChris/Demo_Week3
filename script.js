de52c657a09edd9256f0f14d4d24703789b4988a
Nguyen Tricsie <bddkhoa@gmail.com>
2023-11-18 21:04:40 +0700

    update js and css

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 const displayScreen = document.getElementById("display-screen");
 
+const number1 = document.getElementById("num-1");
+const number2 = document.getElementById("num-2");
+const number3 = document.getElementById("num-3");
+const number4 = document.getElementById("num-4");
+const number5 = document.getElementById("num-5");
+const number6 = document.getElementById("num-6");
 const number7 = document.getElementById("num-7");
 const number8 = document.getElementById("num-8");
+const number9 = document.getElementById("num-9");
 
 const sumBtn = document.getElementById("sum-btn");
+
+number1.onclick = function () {
+  displayScreen.value += '1';
+};
+
+number2.onclick = function () {
+  displayScreen.value += '2';
+};
+
+number3.onclick = function () {
+  displayScreen.value += '3';
+};
+
+number4.onclick = function () {
+  displayScreen.value += '4';
+};
+
+number5.onclick = function () {
+  displayScreen.value += '5';
+};
+
+number6.onclick = function () {
+  displayScreen.value += '6';
+};
 
 number7.onclick = function () {
   displayScreen.value += '7';
@@ -13,11 +44,25 @@ number8.onclick = function () {
   displayScreen.value += '8';
 };
 
+number9.onclick = function () {
+  displayScreen.value += '9';
+};
+
 sumBtn.onclick = function () {
-  if(displayScreen.innerHTML == ''){
-    displayScreen.value = '+';
-  }
-  else{
-    displayScreen.value += '+';
-  }
+  displayScreen.value += '+';
+};
+sumBtn.onclick = function () {
+  displayScreen.value += '+';
+};
+
+sumBtn.onclick = function () {
+  displayScreen.value += '+';
+};
+
+sumBtn.onclick = function () {
+  displayScreen.value += '+';
+};
+
+sumBtn.onclick = function () {
+  displayScreen.value += '+';
 };
